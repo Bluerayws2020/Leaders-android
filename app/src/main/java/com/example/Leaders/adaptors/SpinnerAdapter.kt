28707a71f2864e3ec000f697bb39de87e05e18ba
ayhamhammd <109752673+ayhamhammd@users.noreply.github.com>
2023-04-01 20:03:00 +0300

    this version commited by ayham hammad

--- app/src/main/java/com/example/Leaders/adaptors/SpinnerAdapter.kt
+++ app/src/main/java/com/example/Leaders/adaptors/SpinnerAdapter.kt
@@ -15,7 +15,6 @@ class SpinnerAdapter(context: Context, items: List<Student>) :
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context)
             .inflate(android.R.layout.simple_spinner_dropdown_item, parent, false)
-
         val item = getItem(position)
         view.findViewById<TextView>(android.R.id.text1).text = item?.full_name
 
